61004a08934eb04358bfc07601f1d7c070960d27
Paul Hoskinson <plhosk@gmail.com>
2016-11-22 17:35:48 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var passport = require("passport");
+
+var app = express();
+require("./passport")();
+
+const MONGO_URI = process.env.MONGO_URI
+mongoose.connect(MONGO_URI);
+
+
+app.set("port", process.env.PORT || 5000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: "very important secret",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('public'));
+app.use('/signup', require('./api/signup'));
+app.use('/login', require('./api/login'));
+
+app.listen(app.get("port"), function () {
+  console.log("Server started on port " + app.get("port"));
+});
