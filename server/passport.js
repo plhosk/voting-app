31bd04256df5c39b7ddc21ca3b057963c2fcf40c
Paul Hoskinson <plhosk@gmail.com>
2016-11-29 22:39:18 -0700

    add github authentication

--- server/passport.js
+++ server/passport.js
@@ -37,7 +37,7 @@ var GitHubStrategy = require('passport-github2').Strategy
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    callbackURL: "https://voting-app-plhosk.herokuapp.com/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOrCreate({ githubId: profile.id }, function (err, user) {
