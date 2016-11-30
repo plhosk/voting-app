9a7b1758bdb2a3bc155109bd8e65a62a135af8e1
Paul Hoskinson <plhosk@gmail.com>
2016-11-29 22:52:28 -0700

    fix github auth

--- server/api/github.js
+++ server/api/github.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var passport = require('passport')
 
-var router = express.Router()
+var router = express.Router({ mergeParams: true })
 
 router.route('/')
   .get(passport.authenticate('github', {scope: [ 'user:email' ] }))
